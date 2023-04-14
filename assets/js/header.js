ed9d286f25eb89dacda1a3bdcd13a33397f1ded4
Abi0l9 <cryptog3ms@gmail.com>
2023-04-14 10:06:55 +0100

    task completed for header and main section

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,15 +1,23 @@
 const headerIcon = document.querySelector(".header-icon");
 const toggle = document.querySelector(".toggle");
 const headerMenu = document.querySelector(".header-menu");
+const menuItems = document.getElementsByClassName("menu-item");
+
 let display = false;
 
 const toggleMenu = () => {
   if (!display) {
     display = true;
+
     headerMenu.style.left = "0";
+    headerMenu.style.transition =
+      "transform 0.5s cubic-bezier(0.8, 0, 0.16, 1)";
   } else if (display) {
     display = false;
+
     headerMenu.style.left = "100%";
+    headerMenu.style.transition =
+      "transform 0.5s cubic-bezier(0.8, 0, 0.16, 1)";
   }
 };
 
